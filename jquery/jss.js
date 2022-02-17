4932b8fc2c4f8a984aa3f94dd3e9e933f9b62e65
ayshanurseyt <96568617+ayshanurseyt@users.noreply.github.com>
2022-02-17 15:50:52 +0600

    Add files via upload

---
+++ jquery/jss.js
@@ -0,0 +1,7 @@
+function Generate(){
+    var names=document.getElementById('name').value;
+
+        var randName = names[Math.floor(Math.random() * names.length)];
+
+        document.getElementById("result").innerHTML=names[randName];
+   }
